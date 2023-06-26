4b8810fc677bf0daed115119d7e278768818cd81
woals7480 <94383932+woals7480@users.noreply.github.com>
2023-06-27 00:59:35 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const debug = process.env.NODE_ENV !== "production";
-const repository = "joonyoung-s-portfolio-page";
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  assetPrefix: !debug ? `/${repository}/` : "",
+  basePath: "/my-portfolio",
 };
 
 module.exports = nextConfig;

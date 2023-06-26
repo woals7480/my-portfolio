d52059107a85baecc3f5b99e972e2d9db4d34d49
woals7480 <94383932+woals7480@users.noreply.github.com>
2023-06-27 01:13:28 +0900

    Updates

---
+++ _next/static/vqWD6jSFEgzvLxBnRXs3D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/303-b25741aa8e461832.js","static/css/7f5c38cc0eca3a91.css","static/chunks/pages/index-94ed8e5564170e57.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
